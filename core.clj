0bcac7ecc0da6a3d9da4135998e5bf9be7742d33
Pablo Botelho <pablo.bcb@gmail.com>
2018-05-18 17:26:11 -0300

    Add mechanism to match wildcard

--- core.clj
+++ core.clj
@@ -3,15 +3,22 @@
 (ns slip-api.with
   (:require [clojure.core.match :refer [match]]))
 
+
 (defmacro with [bindings & rest]
   (assert vector? bindings)
   (let [[body else-bindings] (split-with #(not (keyword? %)) rest)
         else-bindings (next else-bindings)
-        [clause val] (first (partition 2 bindings))
+        [clause expr] (first (partition 2 bindings))
+        value (eval expr)
         [clause* then] (first (partition 2 else-bindings))]
-    `(match ~val
+    `(match '~value
             ~clause (do ~@body)
-            ~clause* ~then)))
+
+            ~clause* ~then
+
+            ~'else ~'else
+
+            )))
 
 #_(macroexpand-1 '(with [[n 1] [1 1]]
                         (+ n 1)
