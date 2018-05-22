8bb6b140910205486a5bfa91b25bd70e5a3a4ab7
Pablo Botelho <pablo.bcb@gmail.com>
2018-05-22 15:03:18 -0300

    Update core.clj

--- core.clj
+++ core.clj
@@ -1,46 +1,51 @@
-(ns clj-with.core)
-
 (ns slip-api.with
-  (:require [clojure.core.match :refer [match]]))
-
+  (:require
+    [clojure.walk :refer :all]
+    [clojure.core.match :refer [match]]))
 
 (defn ^:private match* [body bindings else-bindings]
   (if (empty? bindings)
-    (do ~@body)
+    (do  (prn "ASSADSD")
+      body)
     (let [[clause expr] (first bindings)
-          value (eval expr)]
+          value (eval expr)
+          x 4]
       `(match '~value
-         ~clause
-          ~'body
-         ;(match* ~body ~(next bindings) ~else-bindings)
+              ~clause
+              ;~(make-add x)
+              ~(match* body (next bindings) else-bindings)
 
-         ~'else
-         (match ~'else
-           ~@else-bindings)
-         ))))
+              ~'else
+              (match ~'else
+                     ~@else-bindings)))))
 
 (defmacro with [bindings & rest]
   (assert vector? bindings)
   (let [[body else-bindings] (split-with #(not (keyword? %)) rest)
-        else-bindings (next else-bindings)]
-    (match* (first body) (partition 2 bindings) else-bindings)))
+        else-bindings (next else-bindings)
+        res (match* (first body) (partition 2 bindings) else-bindings)]
+    (clojure.pprint/pprint res)
+    res
+    ))
 
 
-#_(macroexpand-1 '(with [[n 1] [1 1]]
-                        (+ n 1)
-  
-                        :else
-                        [:error data] :ok)
-                         
-                        wildcard :wildcard)
+(macroexpand-1
+  '(with [[n 1] [1 1]
+         [m] (* 2 n)]
+        (+ m 1)
 
+        ;:else
+        ;~[:error data] :ok
+        ;
+        ;wildcard :wildcard
+        )
+  )
+(with [[n 1] [1 1]
+       [m] (* 2 n)]
+      (+ m 1)
 
-
-(defn -main
-  "Hello `with`"
-  []
-  (with [[n 1] [10 1]]
-      (+ n 1)
-
-      :else
-      [:error data] :ok))
+      ;:else
+      ;~[:error data] :ok
+      ;
+      ;wildcard :wildcard
+      )
