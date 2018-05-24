d87f961b9e3539ad6595805ea72e7120a205d023
Pablo Botelho <pablo.bcb@gmail.com>
2018-05-24 19:56:21 -0300

    First working version :)

--- core.clj
+++ core.clj
@@ -5,14 +5,13 @@
 
 (defn ^:private match* [body bindings else-bindings]
   (if (empty? bindings)
-    (do  (prn "ASSADSD")
-      body)
-    (let [[clause expr] (first bindings)
-          value (eval expr)
-          x 4]
-      `(match '~value
+    body
+    (let [[clause expr] (first bindings)]
+      `(match (let [val# (eval ~expr)]
+                (if (vector? val#)
+                  '~expr
+                  [val#]))
               ~clause
-              ;~(make-add x)
               ~(match* body (next bindings) else-bindings)
 
               ~'else
@@ -22,30 +21,15 @@
 (defmacro with [bindings & rest]
   (assert vector? bindings)
   (let [[body else-bindings] (split-with #(not (keyword? %)) rest)
-        else-bindings (next else-bindings)
-        res (match* (first body) (partition 2 bindings) else-bindings)]
-    (clojure.pprint/pprint res)
-    res
-    ))
+        else-bindings (next else-bindings)]
+    (match* (first body) (partition 2 bindings) else-bindings)))
 
 
-(macroexpand-1
-  '(with [[n 1] [1 1]
-         [m] (* 2 n)]
-        (+ m 1)
-
-        ;:else
-        ;~[:error data] :ok
-        ;
-        ;wildcard :wildcard
-        )
-  )
-(with [[n 1] [1 1]
+#_(with [[n 1] [10 1]
        [m] (* 2 n)]
       (+ m 1)
 
-      ;:else
-      ;~[:error data] :ok
-      ;
-      ;wildcard :wildcard
-      )
+      :else
+      [:error data] :ok
+
+      wildcard :wildcard)
