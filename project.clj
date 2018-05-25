ba33593d461e9e782c0c98841fca84b4c3f1c0f0
Pablo Botelho <pablo.bcb@gmail.com>
2018-05-25 00:32:48 -0300

    Transformed repo into lein project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clj-with "0.1.0-SNAPSHOT"
+  :description "Macro inspired by Elixir's `with` statement"
+  :url "https://github.com/pablobcb/clj-with"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.match "0.3.0-alpha5"]])
--- src/clj_with/core.clj
+++ src/clj_with/core.clj
@@ -1,7 +1,5 @@
 (ns clj-with.core
-  (:require
-    [clojure.walk :refer :all]
-    [clojure.core.match :refer [match]]))
+  (:require [clojure.core.match :refer [match]]))
 
 (defn ^:private match* [body bindings else-bindings]
   (if (empty? bindings)
@@ -10,22 +8,21 @@
       `(match (let [val# (eval ~expr)]
                 (if (vector? val#) val# [val#]))
               ~clause
+              ;; if to evaluate body goes here
               ~(match* body (next bindings) else-bindings)
 
               ~'else
               (match ~'else
                      ~@else-bindings)))))
 
-(clojure.repl/doc match)
 (defmacro with
-  "Chain pattern matching clauses, using 'clojure.core/match'. Take a vector of
+  "Chain pattern matching clauses, using 'clojure.core.match/match'. Take a vector of
   bindings to match, a body, and optionally an `:else` keyword followed by list
   of clauses to match in case of failure. Pattern matches each bind and if all
   clauses match, the body is executed returning its result. Otherwise the chain
   is aborted and the non-matched value is matched against the optional :else
   clauses, if provided. Raises 'java.lang.IllegalArgumentException' if none
-  of the clauses match, just as 'clojure.core/match'.
-
+  of the clauses match, just as 'clojure.core.match/match'.
   Example:
   (defn f1 [] [:ok 10 20])
   (defn f2 [k] [:ok (inc k)])
@@ -34,21 +31,20 @@
   (with [[:ok x y] (f1)
          [:ok z] (f2 (+ x y))]
     (* z 2)
-
     :else
     [:error err] err)
-    ;=> 62
+  ;=> 62
 
   (with [[:ok x y] (f1)
          [:ok z] (produce-error)]
     (* z 2)
-
     :else
     [:error err] err)
-    ;=> :match-error"
+  ;=> :match-error"
   [bindings & rest]
   (assert vector? bindings)
   (assert (even? (count bindings)))
   (let [[body else-bindings] (split-with #(not (keyword? %)) rest)
         else-bindings (next else-bindings)]
     (match* (first body) (partition 2 bindings) else-bindings)))
+
