2b8af0bde74eca362c277a43242a6c7c230a6391
Pablo Botelho <pablo.bcb@gmail.com>
2018-05-25 00:59:08 -0300

    Add basic tests

--- test/clj_with/core_test.clj
+++ test/clj_with/core_test.clj
@@ -2,6 +2,23 @@
   (:require [clojure.test :refer :all]
             [clj-with.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn f1 [] [:ok 10 20])
+(defn f2 [k] [:ok (inc k)])
+(defn produce-error [] [:error :match-error])
+
+(deftest single-expr-body
+  (is (= 62
+         (with [[:ok x y] (f1)
+                [:ok z] (f2 (+ x y))]
+               (* z 2)
+
+               :else
+               [:error err] err))))
+
+(deftest match-error
+  (= :match-error
+     (with [[:ok x y] (f1)
+            [:ok z] (produce-error)]
+           (* z 2)
+           :else
+           [:error err] err)))
